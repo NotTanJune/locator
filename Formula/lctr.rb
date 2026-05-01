f4f6b4f85194b1ab695486a2ba8ceaf1d0446781
Tanmay Nargas <tanmaynargas291@gmail.com>
2026-05-01 16:50:53 +0800

    Use source tarball in Homebrew formula

--- Formula/lctr.rb
+++ Formula/lctr.rb
@@ -1,8 +1,8 @@
 class Lctr < Formula
   desc "Fast local file metadata search"
   homepage "https://github.com/NotTanJune/locator"
-  url "https://github.com/NotTanJune/locator.git",
-      tag: "v0.1.41"
+  url "https://github.com/NotTanJune/locator/archive/refs/tags/v0.1.41.tar.gz"
+  sha256 "6f1415de83c2f37e0778c3493c14c19faa0c5e828f98c3cb734d2ae0371b3692"
   license "GPL-3.0-only"
   head "https://github.com/NotTanJune/locator.git", branch: "main"
 
